15fa2dcb31400b597c98f3b2ba00f6287c80476f
sri_sameendhran <ssrisameendhran@gmail.com>
2024-11-19 23:03:13 +0530

    commit

---
+++ sam/src/sam/demo.java
@@ -0,0 +1,11 @@
+package sam;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.print("sam");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ sam/src/sam/test.java
@@ -0,0 +1,11 @@
+package sam;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("sam");
+		// TODO Auto-generated method stub
+
+	}
+
+}
