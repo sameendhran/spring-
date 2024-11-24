6d53150344ef406221f155b4035ff3c61455c588
sri_sameendhran <146868947+sameendhran@users.noreply.github.com>
2024-11-24 19:39:59 +0530

    Add files via upload

---
+++ Model and View/springbootWeb1/src/test/java/com/example/springbootWeb/SpringbootWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
